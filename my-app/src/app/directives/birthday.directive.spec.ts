f7ba8c94e4a5785786db2500a6cdb91f5251d5ce
Brianna Price <briannaprice@Briannas-MacBook-Air.local>
2020-02-14 14:59:29 -0600

    first commit

--- my-app/src/app/directives/birthday.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BirthdayDirective } from './birthday.directive';
-
-describe('BirthdayDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BirthdayDirective();
-    expect(directive).toBeTruthy();
-  });
-});
