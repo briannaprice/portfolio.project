a208d0af6bd3667ee29d9a50cac79d81f2f33532
Brianna Price <briannaprice@Briannas-MacBook-Air.local>
2020-02-17 14:04:48 -0600

    changes

--- APPY/src/app/app-routing.module.ts
+++ APPY/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+import { UserComponent } from './user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomeComponent},
+  {path: 'post/:id', component: PostComponent},
+  {path: 'user/:id', component: UserComponent},
+  {path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
