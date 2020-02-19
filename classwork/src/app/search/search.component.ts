475a67dd8b4d10eb6d522cf314431e95f0a2323f
Brianna Price <briannaprice@Briannas-MacBook-Air.local>
2020-02-19 11:01:47 -0600

    changes

--- classwork/src/app/search/search.component.ts
+++ classwork/src/app/search/search.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Query } from '@angular/core';
 import { GifService } from '../services/gif.service';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/internal/operators/map';
+import { filter, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -11,11 +14,18 @@ export class SearchComponent implements OnInit {
   constructor(private gifService: GifService) { }
 
   //searchGifs(){
-    //this.gifService.gifsByQuery(this.searchString).subscribe(gifResponse => this.gifs = gifResponse)
+    //this.gifService.gifsByQuery(this.searchString).subscribe(gif => this.gifs = gif)
   //}
 
   ngOnInit(): void {
-    const searchInput = document.getElementById("searchField");
+    const searchField = document.getElementById("searchField");
+    const searchObservable = fromEvent(searchField, "keyup").pipe(
+      map(e=>e.target['value']),
+      filter(searchString => searchString.length > 3),
+      debounceTime(400),
+      distinctUntilChanged()
+    );
+    searchObservable.subscribe(query => this.gifService.gifsByQuery(query).subscribe(gif => this.gifs = gif));
   }
 
 }
--- classwork/src/app/services/gif.service.ts
+++ classwork/src/app/services/gif.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {map} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifService {
-  gifsByQuery(searchString: string) {
-    throw new Error("Method not implemented.");
+
+  constructor(private http: HttpClient) { }
+
+  gifsByQuery(searchString){
+    return this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=DtwTkZwAIfcbYylnIlpb5tczU7Y3MH8n&q=${searchString}&limit=25&offset=0&rating=R&lang=en`)
+    .pipe(
+      map(res => res["data"])
+    )
   }
-  
-  constructor() { }
+
 }
